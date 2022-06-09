9e02ffa0349addb8e71aeb4d776f4ce47d9a3292
akucina2001 <andchg.10@gmail.com>
2022-06-09 21:36:22 +0700

    Co-authored-by: rfvin <rfvin@users.noreply.github.com>

--- src/halaman/masuk.tsx
+++ src/halaman/masuk.tsx
@@ -4,7 +4,7 @@ import '../index.css'
 const masuk = () => {
     return (
         <div>
-            <p className="title">Masuk</p>
+            <p className="title-masuk">Masuk</p>
             <input type="text" placeholder="Password" />
             <button>Masuk</button>
         </div>
