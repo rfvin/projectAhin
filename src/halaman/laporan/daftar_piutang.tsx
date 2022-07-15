33c169aaace521a94d8ad6bc19fb870182116f99
rfvin <rfvinzii@gmail.com>
2022-07-15 14:57:19 +0700

    Daftar piutang (done)

--- src/halaman/laporan/daftar_piutang.tsx
+++ src/halaman/laporan/daftar_piutang.tsx
@@ -1,16 +1,162 @@
-import React,{FC} from "react";
-import {collection,getDocs,doc,deleteDoc,addDoc} from "firebase/firestore";
+import React,{FC,useEffect,useState} from "react";
+import {collection,getDocs,doc,deleteDoc,addDoc, getDoc} from "firebase/firestore";
 import {db} from "../../firebase";
 import moment from "moment";
 import Transaksi from "../transaksi";
 
 const DaftarPiutang : FC = () =>{
-    const daftarPiutangRef = collection(db,'daftar_piutang')
-    
+    const daftarPiutangRef = collection(db,'daftar_piutang1')
+    const riwayatPembelianCollectionRef = collection(db,'riwayat_pembelian')
+    const [dataPiutang,setDataPiutang] = useState<any>([])
+    const [itemTransaksi,setItemTransaksi] = useState<any>()
+    const [transaksi,setTransaksi] = useState<any>()
+    const[objectPiutangSelected,setObjectPiutangSelected] = useState<any>(
+        {
+            id: ''
+        }
+    )
+
+    const getDaftarPiutang = async () => {
+        await getDocs(daftarPiutangRef)
+            .then(res => {
+                setDataPiutang([...res.docs.map(doc => [doc.data(),doc.id])])
+            })
+    }
+
+    const hapusPiutang =async () => {
+        const DaftarPiutangRef = doc(db,'daftar_piutang1', objectPiutangSelected.id)
+        await deleteDoc(DaftarPiutangRef)
+            .then((res)=>{
+                window.location.reload()
+            })
+    }
+
+    const setLunasPiutang = async () => {
+        await addDoc(riwayatPembelianCollectionRef,{
+            kode_transaksi: transaksi.kode_transaksi,
+            nama_pembeli: transaksi.nama_pembeli,
+            keterangan: transaksi.keterangan,
+            total_harga_transaksi: transaksi.total_harga_transaksi
+        }).then(() =>{
+            hapusPiutang()
+        })
+    }
+
+    const getTransaksi =async (obj:any) => {
+        setObjectPiutangSelected((prev:any) => ({
+            ...prev,
+            id: obj.id
+        }))
+        const TransaksiRef = collection(db,'transaksi', obj.kode_transaksi,'item')
+        await getDocs(TransaksiRef)
+            .then((res)=>{
+                setItemTransaksi(res.docs.map(doc => doc.data()))
+                setTransaksi(obj)
+            })
+
+    }
+    useEffect(()=>{
+        getDaftarPiutang()
+    },[])
     
     return(
         <div className="row">
             
+            <div className="col-12 mt-4">
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Tanggal Transaksi</th>
+                            <th>Total Harga</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            dataPiutang.length > 0 &&
+                            dataPiutang.map((item:any) => {
+                                item[0].id = item[1]
+
+                                return(
+                                    <tr key={item[0].kode_transaksi}>
+                                        <td>{moment(item[0].kode_transaksi.split('#')[0]).format('MMMM Do YYYY, h:mm:ss a')}</td>
+                                        <td>Rp. {item[0].total_harga_transaksi}</td>
+                                        <td>
+                                            <a href="#!" onClick={()=>{getTransaksi(item[0])}} data-bs-toggle="modal" data-bs-target="#lihatRiwayatPembelian">Lihat</a>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+            {/* Modal Pop Up Lihat Riwayat Pembelian */}
+                        <div className="modal fade" id="lihatRiwayatPembelian" tabIndex={-1} aria-labelledby="lihatRiwayatPembelianLabel" aria-hidden="true">
+                            <div className="modal-dialog modal-lg">
+                                <div className="modal-content">
+                                    <div className="modal-header blue text-white">
+                                        <h5 className="modal-title" id="lihatRiwayatPembelianLabel">RiwayatPembelian</h5>
+                                        <button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <div className="row">
+                                            <div className="col-6">
+                                                <b>Nama Pembeli : </b> <br/>
+                                                {itemTransaksi && moment(itemTransaksi[0].item.tanggal.transaksi).format('MMMM Do YYYY')}
+                                            </div>
+                                            <div className="col-6">
+                                                <b>Tanggal Transaksi :</b>
+                                            </div>
+                                            <div className="col-12 mt-4">
+                                                <b>Keterangan</b> <br/>
+                                                {transaksi && transaksi.keterangan}
+                                            </div>
+                                        </div>
+                                        <div className="row mt-5">
+                                            <div className="table-responsive">
+                                                <table className="table table-bordered">
+                                                    <thead>
+                                                        <tr>Nama Barang</tr>
+                                                        <tr>Kode barang</tr>
+                                                        <tr>Harga</tr>
+                                                        <tr>Stok</tr>
+                                                        <tr>Jumlah Beli</tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        {
+                                                            itemTransaksi &&
+                                                            itemTransaksi.map((item:any)=>{
+                                                                return(
+                                                                    <tr key={item.item.kode_barang}>
+                                                                        <td>{item.item.kode_barang}</td>
+                                                                        <td>{item.item.nama_barang}</td>
+                                                                        <td>{item.item.harga_barang}</td>
+                                                                        <td>{item.item.jumlah_barang}</td>
+                                                                        <td>{item.item.jumlah_beli}</td>
+                                                                    </tr>
+                                                                )
+                                                            })
+                                                        }
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-12">
+                                                <h3>Total Harga : Rp. {transaksi && transaksi.total_harga_transaksi}</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" onClick={() => {hapusPiutang()}} className="btn red text-white">Hapus</button>
+                                        <button type="button" onClick={() => {setLunasPiutang()}} className="btn green text-white">Lunas</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
         </div>
     )
 
